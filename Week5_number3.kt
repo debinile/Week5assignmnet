53dee6f15224b181ddcbc9579b6c182687b88e61
debinile <85246573+debinile@users.noreply.github.com>
2021-07-05 23:49:58 +0100

    Create Week5_number3.kt

---
+++ Week5_number3.kt
@@ -0,0 +1,16 @@
+fun removeVowels(s: String): String {
+    val re = StringBuilder()
+    for (c in s) {
+        if (c != 'A' && c != 'a'
+            && c != 'E' && c != 'e'
+            && c != 'I' && c != 'i'
+            && c != 'O' && c != 'o'
+            && c != 'U' && c != 'u') {
+            re.append(c)
+        }
+    }
+    return re.toString()
+}
+
+fun main() {
+    println(removeVowels("Kotlin Programming Language"))
